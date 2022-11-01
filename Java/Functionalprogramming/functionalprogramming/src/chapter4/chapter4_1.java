dd3c12e0348cab35ed0c3bb94dcbfa6efb0cfdb6
leewg97 <leewg97@naver.com>
2022-11-01 22:33:03 +0900

    Functional Interface

---
+++ Java/Functionalprogramming/functionalprogramming/src/chapter4/chapter4_1.java
@@ -0,0 +1,17 @@
+package chapter4;
+
+import java.util.function.Supplier;
+
+public class chapter4_1 {
+
+    public static void main(String[] args) {
+        Supplier<Double> doubleSupplier = () -> Math.random();
+        printRandomDoubles(doubleSupplier, 5);
+    }
+
+    public static void printRandomDoubles(Supplier<Double> randomSupplier, Integer count) {
+        for (int i = 0; i < count; i++) {
+            System.out.println(randomSupplier.get());
+        }
+    }
+}
---
+++ Java/Functionalprogramming/functionalprogramming/src/chapter4/model/User.java
@@ -0,0 +1,29 @@
+package chapter4.model;
+
+public class User {
+
+    private Integer id;
+    private String name;
+
+    public User(Integer id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+
+}
