cb2aeace15cb4344225d3318535d450fdfc2811e
leewg97 <leewg97@naver.com>
2022-11-03 06:23:40 +0900

    Optional

--- Java/Functional_Programming/functionalprogramming/src/optional/Chapter7_2.java
+++ Java/Functional_Programming/functionalprogramming/src/optional/Chapter7_2.java
@@ -1,9 +1,39 @@
 package optional;
 
+import optional.model.User;
+
+import java.util.Optional;
+
 public class Chapter7_2 {
 
     public static void main(String[] args) {
+        Optional<User> maybeUser = Optional.ofNullable(maybeGetUser(true));
+        maybeUser.ifPresent(System.out::println);
 
+        Optional<Integer> maybeId = Optional.ofNullable(maybeGetUser(true))
+                .map(User::getId);
+        maybeId.ifPresent(System.out::println);
+
+        String userName = Optional.ofNullable(maybeGetUser(false))
+                .map(User::getName)
+                .map(name -> "The name is " + name)
+                .orElse("Name is empty");
+        System.out.println(userName);
+
+        Optional<String> maybeEmail = Optional.ofNullable(maybeGetUser(true))
+                .flatMap(User::getEmailAddress);
+        maybeEmail.ifPresent(System.out::println);
+    }
+
+    public static User maybeGetUser(boolean returnUser) {
+        if (returnUser) {
+            return new User()
+                    .setId(1001)
+                    .setName("Chris")
+                    .setEmailAddress("chris@gmail.com")
+                    .setVerified(false);
+        }
+        return null;
     }
 
 }
