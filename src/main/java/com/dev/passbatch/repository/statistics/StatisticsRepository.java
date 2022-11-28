06bde93805a90271ad7ddffd119b5ab36ac284b1
leewg97 <leewg97@naver.com>
2022-11-29 04:44:13 +0900

    Feat #1 : Add StatisticsEntity

---
+++ src/main/java/com/dev/passbatch/repository/statistics/StatisticsRepository.java
@@ -0,0 +1,6 @@
+package com.dev.passbatch.repository.statistics;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StatisticsRepository extends JpaRepository<StatisticsEntity, Integer> {
+}
