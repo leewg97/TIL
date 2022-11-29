b374d4f16ae08578878dcc8d2e4719786fb42eda
leewg97 <leewg97@naver.com>
2022-11-30 02:13:11 +0900

    Gradle setting

---
+++ src/test/java/com/dev/passweb/PtLicenseWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.passweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PtLicenseWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
