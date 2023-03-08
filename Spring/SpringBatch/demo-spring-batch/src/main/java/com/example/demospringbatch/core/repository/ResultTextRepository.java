28e19f378afad69ae2329754674a11ef0b995b08
leewg97 <leewg97@naver.com>
2023-03-09 04:08:16 +0900

    SprintBatch Example project deleted

--- Spring/SpringBatch/demo-spring-batch/src/main/java/com/example/demospringbatch/core/repository/ResultTextRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demospringbatch.core.repository;
-
-import com.example.demospringbatch.core.domain.ResultText;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ResultTextRepository extends JpaRepository<ResultText, Integer> {
-}
