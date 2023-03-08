f7715e661c2a3a522bc13055bc7c1b0e25ca0a83
leewg97 <leewg97@naver.com>
2023-03-09 04:09:21 +0900

    Spring Security Example project deleted

--- Spring/SpringSecurity/Jwt/src/main/java/com/won/tutorial/repository/AuthorityRepository.java
+++
@@ -1,7 +0,0 @@
-package com.won.tutorial.repository;
-
-import com.won.tutorial.entity.Authority;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AuthorityRepository extends JpaRepository<Authority, String> {
-}
--- Spring/SpringSecurity/oauth2/src/main/java/com/example/oauth2/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.oauth2.repository;
-
-import com.example.oauth2.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    boolean existsByEmail(String email);
-}
